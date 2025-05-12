3de0e9c3df1e3f08e4ed82d5d851f2425048d8ad
Stanley Cao <stanley.l.cao@gmail.com>
2025-05-12 00:16:36 -0700

    final version

--- Assets/Scripts/BirdScript.cs
+++ Assets/Scripts/BirdScript.cs
@@ -3,21 +3,42 @@ using UnityEngine.InputSystem;
 
 public class BirdScript : MonoBehaviour
 {
+    private Camera mainCamera;
     public Rigidbody2D myRigidbody;
     public float flapStrength;
+    public LogicScript logic;
+    public bool birdIsAlive = true;
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        mainCamera = Camera.main;
+        logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<LogicScript>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Keyboard.current.spaceKey.wasPressedThisFrame)
+        if (Keyboard.current.spaceKey.wasPressedThisFrame && birdIsAlive)
         {
             myRigidbody.linearVelocity = Vector2.up * flapStrength;
         }
+
+        // Convert the bottom edge of the screen to world coordinates
+        float screenBottom = mainCamera.ViewportToWorldPoint(new Vector3(0, 0, transform.position.z)).y;
+
+        // Check if object has fallen below the bottom of the screen
+        if (transform.position.y < screenBottom)
+        {
+            logic.gameOver();
+            birdIsAlive = false;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        logic.gameOver();
+        birdIsAlive = false;
     }
 }
